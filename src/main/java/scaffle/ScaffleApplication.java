5b741e6e28e82b1226b515c57b39272c9e5c062b
mizurova <mizurova@gmail.com>
2015-03-08 19:22:14 +0200

    Add example project

---
+++ src/main/java/scaffle/ScaffleApplication.java
@@ -0,0 +1,12 @@
+package scaffle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScaffleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScaffleApplication.class, args);
+    }
+}
