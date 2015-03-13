466c207e762861031cdb34551ba6a8435942dd01
mizurova <mizurova@gmail.com>
2015-03-14 01:22:02 +0200

    Add Book entity

---
+++ src/main/java/scaffle/domain/Book.java
@@ -0,0 +1,42 @@
+package scaffle.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+/**
+ * Created by Iryna on 14.03.2015.
+ */
+@Entity
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private long id;
+    private String author;
+    private String title;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
