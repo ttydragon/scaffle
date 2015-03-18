50cf1e5ddf1f6be24b276871cac24c4f747deee9
mizurova <mizurova@gmail.com>
2015-03-18 22:11:05 +0200

    remove unused imports

--- src/test/java/scaffle/BookControllerTest.java
+++ src/test/java/scaffle/BookControllerTest.java
@@ -1,14 +1,11 @@
 package scaffle;
 
-import javafx.application.Application;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.context.embedded.EmbeddedWebApplicationContext;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.http.MediaType;
-import org.springframework.mock.web.MockServletContext;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
